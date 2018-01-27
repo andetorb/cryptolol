31b60543e510e08938b9331b4a2806bbfd910b59
anters <anters@localhost.localdomain>
2018-01-27 22:21:40 +0100

    first commit

---
+++ coins.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+# BTC = Bitcoin
+# XMR = Monero
+# ZEC = zCash
+# IOT = IOTA
+# ETH = Ethereum
+
+val=( BTC ETH XMR ZEC IOT)
+value=( bitcoin ethereum zcash)
+
+# Usage message
+usage() { echo "Usage: $0 [-c|-p <cli.fyi|coinmarketcap>]" 1>&2; exit 1; }
+
+# Catch 0 arguments
+if [[ $1 == "" ]]; 
+then
+	usage;
+	exit;
+fi
+# check for options -c and -p
+while getopts "cp" OPTION; do
+    case $OPTION in
+    c)
+        for i in "${val[@]}"
+        do
+                verdi=$(curl -s cli.fyi/"$i" | jq '.data."USD ($)"')
+                echo $i "er på" $verdi "usd"
+        done
+        ;;
+    p)
+        for i in "${value[@]}"
+        do
+                #verdi=$(curl -s https://api.coinmarketcap.com/v1/ticker/"$i"/)
+                #echo $verdi | jq -r '.[0].price_usd, .[0].percent_change_1h ,.[0].percent_change_24h, .[0].percent_change_7d'
+                verdi1=$(curl -s https://api.coinmarketcap.com/v1/ticker/"$i"/ | jq -r '.[0].price_usd')
+                verdi2=$(curl -s https://api.coinmarketcap.com/v1/ticker/"$i"/ | jq -r '.[0].percent_change_1h')
+                verdi3=$(curl -s https://api.coinmarketcap.com/v1/ticker/"$i"/ | jq -r '.[0].percent_change_24h')
+                verdi4=$(curl -s https://api.coinmarketcap.com/v1/ticker/"$i"/ | jq -r '.[0].percent_change_7d')
+                echo "["$i"]" "USD:" $verdi1 "1h:" $verdi2 "24h:" $verdi3 "7d:" $verdi4 
+        done
+        ;;
+    *)
+	usage
+        ;;
+    esac
+done
+
